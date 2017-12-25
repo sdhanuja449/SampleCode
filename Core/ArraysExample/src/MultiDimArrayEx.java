532772b9f4bd9fef62d0e38192b224f57541d524
dhanuja <dhanujasandapola@gmail.com>
2017-12-25 18:19:33 -0500

    adding files related to multi-dimentional array in java

---
+++ Core/ArraysExample/src/MultiDimArrayEx.java
@@ -0,0 +1,15 @@
+
+public class MultiDimArrayEx {
+	public static void main(String[] args) {
+        String[][] names = {
+            {"Mr.Smith"},
+            {"Smith", "Jones"}
+        };
+        // Mr. Smith
+     //   System.out.println(names[0][0] + names[1][0]);
+        // Ms. Jones
+     //   System.out.println(names[0][2] + names[1][1]);
+        System.out.println("length of 1st array element in multidimensional array  is:"+names[0].length);
+    }
+
+}
